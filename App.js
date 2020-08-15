15df1d9305396e976b62f625e83e79b45d1b3f22
pankaj.corp <pankaj.corp@gmail.com>
2020-08-15 16:37:13 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,27 @@
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+
+export default function App() {
+  const [text, setText] = useState("Hello World!");
+
+  return (
+    <View style={styles.container}>
+      <Text>{text}</Text>
+      <Button
+        title="Click Me"
+        onPress={() => setText("Hi There! You clicked button.")}
+      />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
